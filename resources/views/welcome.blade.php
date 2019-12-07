ed35567c4687a7e1070470cd9e9343df15ebc4e4
enarcta4010 <paulryan.banilad@gmail.com>
2019-12-07 14:20:08 +0800

    Change Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelasdf
                 </div>
 
                 <div class="links">
